87a239ec52f2aa2f7a9aa6df8fe6219d377fa1ef
1LP <44363412+1-L-P@users.noreply.github.com>
2020-07-12 18:42:22 +1000

    Updates

---
+++ precache-manifest.0b4e6d5e94b765fb3c1b3f2900f4d888.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7ca0802210233fba1158847b7f9190",
+    "url": "/gift-card-bank/index.html"
+  },
+  {
+    "revision": "4281cd993c92fb02c549",
+    "url": "/gift-card-bank/static/css/main.2efd8b90.chunk.css"
+  },
+  {
+    "revision": "df468d469b3c4411cc98",
+    "url": "/gift-card-bank/static/js/2.3dbc19bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gift-card-bank/static/js/2.3dbc19bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4281cd993c92fb02c549",
+    "url": "/gift-card-bank/static/js/main.d2cfc976.chunk.js"
+  },
+  {
+    "revision": "9f986ca3569ae36fc74d",
+    "url": "/gift-card-bank/static/js/runtime-main.3e4041d9.js"
+  },
+  {
+    "revision": "10933849702c7ccd0891f2f3c5a8ef09",
+    "url": "/gift-card-bank/static/media/Icons-1.10933849.svg"
+  },
+  {
+    "revision": "c3fae4dd41aab43f96ef39d704b7ed14",
+    "url": "/gift-card-bank/static/media/Icons-2.c3fae4dd.svg"
+  },
+  {
+    "revision": "14b76718db8ea2083f44787e3831376b",
+    "url": "/gift-card-bank/static/media/Icons-3.14b76718.svg"
+  },
+  {
+    "revision": "ac4be5676dc40fbf6666f050463f3ecf",
+    "url": "/gift-card-bank/static/media/Icons-4.ac4be567.svg"
+  },
+  {
+    "revision": "555f33ae1f3f1bbdfe56ff5d8bc8f452",
+    "url": "/gift-card-bank/static/media/card-logos.555f33ae.png"
+  },
+  {
+    "revision": "1a0789a39ccfa87f465fbc6c0303cf1c",
+    "url": "/gift-card-bank/static/media/eligibility.1a0789a3.png"
+  },
+  {
+    "revision": "01b2349cc1593fafacb9464da6d1201a",
+    "url": "/gift-card-bank/static/media/gcb-logo.01b2349c.png"
+  },
+  {
+    "revision": "b1117c81f0390330ece85afc524be3b2",
+    "url": "/gift-card-bank/static/media/gcb-logo.b1117c81.svg"
+  },
+  {
+    "revision": "cf27e5aef7d8427689b5882cc7dfb0c3",
+    "url": "/gift-card-bank/static/media/hero-illustration.cf27e5ae.png"
+  },
+  {
+    "revision": "5ea0f1a4517a4c19508b8990b3bed6ef",
+    "url": "/gift-card-bank/static/media/partners.5ea0f1a4.png"
+  }
+]);
